b9718883464121404e6ee4dcb247356aa570eb24
Erin-Corrado <e.corrado144@gmail.com>
2014-06-24 16:02:31 -0400

    updated controls. works much better now

--- navLeapEEG.py
+++ navLeapEEG.py
@@ -4,47 +4,9 @@ import gevent
 import pygame
 import Leap, sys
 
-class LeapListener(Leap.Listener):
   
-  def on_init(self, controller):
-    self.drone = libardrone.ARDrone()
-    self.drone.speed = 0.2
-    
-    
-  def on_connect(self, controller):
-    print "Connected"
-    
-  def on_disconnect(self, controller):
-    print "Disconnected"
-    
-  def on_frame(self, controller):
-    frame = controller.frame()
-    
-    if not frame.hands.is_empty:
-      hand = frame.hands[0]
-      
-      fingers = hand.fingers
-      normal = hand.palm_normal
-      direction = hand.direction
-      
-      print "Hand pitch: %f degrees, roll: %f degrees, yaw: %f degrees" % (
-                direction.pitch * Leap.RAD_TO_DEG,
-                normal.roll * Leap.RAD_TO_DEG,
-                direction.yaw * Leap.RAD_TO_DEG)
-      
-      if normal.roll * Leap.RAD_TO_DEG > 10:
-	self.drone.move_left()
-	print "Move left"
-	
-      elif normal.roll * Leap.RAD_TO_DEG < -10:
-	self.drone.move_right()
-	print "Move right"
-      
-      
-    
-
 def main():
-  listener = LeapListener()
+  drone = libardrone.ARDrone()
   headset = emotiv.Emotiv()
   controller = Leap.Controller()
   
@@ -62,8 +24,7 @@ def main():
   
   isRunning = True
   
-  controller.add_listener(listener)
-  listener.drone.takeoff()
+  drone.takeoff()
     
   while isRunning:
       packet = headset.dequeue()
@@ -154,32 +115,55 @@ def main():
 	  pitchCounter=0
 	  
       if(direction['Yaw'] == 0) and (direction['Pitch'] == 0):
-	listener.drone.hover()
+	drone.hover()
 	
       else:
 	if(direction['Pitch'] == 1):
-	  listener.drone.move_forward()
+	  drone.move_forward()
 	elif(direction['Pitch'] == -1):
-	  listener.drone.move_backward()
+	  drone.move_backward()
 	elif(direction['Yaw'] == 1):
-	  listener.drone.turn_right()
+	  drone.turn_right()
 	elif(direction['Yaw'] == -1):
-	  listener.drone.turn_left()
+	  drone.turn_left()
       
       print "Gyro X: %d Gyro Y: %d" % (packet.gyroX, packet.gyroY)
       print "Yaw: %d Pitch: %d" % (direction['Yaw'], direction['Pitch'])
+      
+      frame = controller.frame()
+      print "got frame"
+      
+      if not frame.hands.is_empty:
+	hand = frame.hands[0]
+	print "got this far"
+      
+	fingers = hand.fingers
+	normal = hand.palm_normal
+	handdirection = hand.direction
+      
+	print "Hand pitch: %f degrees, roll: %f degrees, yaw: %f degrees" % (
+                handdirection.pitch * Leap.RAD_TO_DEG,
+                normal.roll * Leap.RAD_TO_DEG,
+                handdirection.yaw * Leap.RAD_TO_DEG)
+      
+	if handdirection.yaw * Leap.RAD_TO_DEG > 10:
+	  drone.move_left()
+	  print "Move left"
+	
+	elif handdirection.yaw * Leap.RAD_TO_DEG < -10:
+	  drone.move_right()
+	  print "Move right"
 	  
       for event in pygame.event.get():
-	if event.type == pygame.KEYDOWN:
+	 if event.type == pygame.KEYDOWN:
 	  if event.key == pygame.K_SPACE:
-	    listener.drone.land()
-	    isRunning = False
+	      drone.land()
+	      isRunning = False
       
    
   headset.close()
-  controller.remove_listener(listener)
   print "Shutting down...",
-  listener.drone.halt()
+  drone.halt()
   print "Success"
   
 if __name__ == '__main__':
