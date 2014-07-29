d3be1bdce8c6debab46b53944e9c1da55b0dfd18
Erin-Corrado <e.corrado144@gmail.com>
2014-07-29 14:47:39 -0400

    switch cased gestures

--- java/LeapInput.java
+++ java/LeapInput.java
@@ -50,57 +50,67 @@ public class LeapInput extends Listener {
 				
 				Hand hand = frame.hands().get(0);
 				
-				GestureList g = frame.gestures();
-        	    for (Gesture gesture : g)
-        	    {
-        	    	if(gesture.type() == Gesture.Type.TYPE_CIRCLE) 
-        	    	{
-        	    		CircleGesture circle = new CircleGesture(gesture);
-        	    		String clockwiseness;
-        	    		
-        	    		if(circle.pointable().direction().angleTo(circle.normal()) <= Math.PI/2)
-        	    		{
-        	    			clockwiseness = "clockwise";
-        	    		}
-        	    		
-        	    		else
-        	    		{
-        	    			clockwiseness = "counterclockwise";
-        	    		}
-        	    		
-        	    		if(clockwiseness.equals("clockwise"))
-        	    		{
-        	    			cmd.spinRight(speed).doFor(1000).hover();
-        	    			System.out.println("Turn right");
-        	    		}
-        	    		
-        	    		else
-        	    		{
-        	    			cmd.spinLeft(speed).doFor(1000).hover();
-        	    			System.out.println("Turn left");
-        	    		}
-        	    	}
-        	    	
-        	    	else if(gesture.type() == Gesture.Type.TYPE_SWIPE)
-        	    	{
-        	    		SwipeGesture swipe = new SwipeGesture(gesture);
-        	    		Vector swipeDirection = swipe.direction();
-        	    		
-        	    		if(swipeDirection.getX() > 0)
-        	    		{
-        	    			cmd.goRight(speed).doFor(1000).hover();
-        	    			System.out.println("Move right");
-        	    		}
-        	    		
-        	    		else if(swipeDirection.getX() < 0)
-        	    		{
-        	    			cmd.goLeft(speed).doFor(1000).hover();
-        	    			System.out.println("Move left");
-        	    		}
-        	    	}
-        	    }
+				for(Gesture gesture : frame.gestures())
+				{
+					switch (gesture.type()) 
+					{
+			        	case TYPE_CIRCLE:
+			        		CircleGesture circle = new CircleGesture(gesture);
+	        	    		String clockwiseness;
+	        	    		
+	        	    		if(circle.pointable().direction().angleTo(circle.normal()) <= Math.PI/2)
+	        	    		{
+	        	    			clockwiseness = "clockwise";
+	        	    		}
+	        	    		
+	        	    		else
+	        	    		{
+	        	    			clockwiseness = "counterclockwise";
+	        	    		}
+	        	    		
+	        	    		if(clockwiseness.equals("clockwise"))
+	        	    		{
+	        	    			cmd.spinRight(speed).doFor(1000).hover();
+	        	    			System.out.println("Turn right");
+	        	    		}
+	        	    		
+	        	    		else
+	        	    		{
+	        	    			cmd.spinLeft(speed).doFor(1000).hover();
+	        	    			System.out.println("Turn left");
+	        	    		}
+			        		break;
+			        		
+			        	case TYPE_KEY_TAP:
+			        		break;
+			        		
+			        	case TYPE_SCREEN_TAP:
+			        		break;
+			        		
+			        	case TYPE_SWIPE:
+			        		SwipeGesture swipe = new SwipeGesture(gesture);
+	        	    		Vector swipeDirection = swipe.direction();
+	        	    		
+	        	    		if(swipeDirection.getX() > 0)
+	        	    		{
+	        	    			cmd.goRight(speed).doFor(1000).hover();
+	        	    			System.out.println("Move right");
+	        	    		}
+	        	    		
+	        	    		else if(swipeDirection.getX() < 0)
+	        	    		{
+	        	    			cmd.goLeft(speed).doFor(1000).hover();
+	        	    			System.out.println("Move left");
+	        	    		}
+			        		break;
+			        		
+			        	default:
+			        		break;
+			    }
+				}
         	    
-        	    if (hand.grabStrength() >= 0.7 && hand.isLeft())
+        	    
+        	    if (hand.isLeft() && hand.grabStrength() >= 0.7)
         	    {
         	    	System.out.println("Landing");
         	    	isRunning = false;
